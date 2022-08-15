6c457473a116bdbec9bd43643236eff94209a17d
David Petzold <C25David.Petzold@afacademy.af.edu>
2022-08-14 21:17:03 -0600

    Now the outline is done...

--- pex1.c
+++ pex1.c
@@ -17,14 +17,23 @@
 //Prototypes:
 void drawLine(int beginRow, int beginCol, int length, bool isHorizontal); //Draws line of length "length" starting from beginRow, beginCol
 void signWork();    //Signs work with "Created with Curses by David Petzold"
-
+//? Maybe make a color method that does...something...idk yet
 
 // Put Your Main Function Here
 int main(void){
 
+    int startX, startY, length;
+    bool horizontality; //If the line is horizontal == 1...if it is vertical == 0
+
+    //Get user input for the ONE line they are allocated...
+    printf("You may draw only ONE line because the programmer is lazy...\n");
+    scanf("Please enter a starting x-coordinate, starting y-coordingate, and length (in order): %i %i %i", &startX, &startY, &length);
+    scanf("Now enter whether the line is horizontal or not (a \'1\' is horizontal and a \'0\' is vertical): %i", &horizontality);
+
+    //Start curses
     initscr();  //! Required by curses to initialize
 
-    //Some bullshit here...
+    /* SOME BULLSHIT HERE */
 
     //Wait for keystroke then end
     move(23, 10);
