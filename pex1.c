f1acd62e96ebf71145de97d8c1c3624219585f79
David Petzold <C25David.Petzold@afacademy.af.edu>
2022-08-15 13:11:24 -0600

    Performance improvements & addiitonal mode

--- pex1.c
+++ pex1.c
@@ -8,7 +8,7 @@
  * Documentation Statement:  Referenced the PEX1 assignment sheet & "Curses Reference Guide.pdf"
  * ============================================================= */
 
-//! Note to self: (0, 0) is the top left
+//! Note to self: (0, 0) is the top left - window size is 23 (R) x 79 (C)
 
 // Include the Curses library
 #include <ncurses.h>
@@ -16,40 +16,70 @@
 // Color Piar Definition
 #define BLACK_WHITE 1
 #define RED_BLUE 2
+#define WHITE_BLUE 3
 
 
 //Prototypes:
 void drawLine(int beginRow, int beginCol, int length, int isHorizontal); //Draws line of length "length" starting from beginRow, beginCol
 void signWork();    //Signs work with "Created with Curses by David Petzold"
-//? Maybe make a color method that does...something...idk yet
+void initials();    //Draws my initials
 
 // Put Your Main Function Here
 int main(void){
 
+    int artDecision;
     int startX, startY, length, horizontality; //If the line is horizontal == 1...if it is vertical == 0
 
-    //Get user input for the ONE line they are allocated...
-    printf("You may draw only ONE line because \"art\" is subjective and the programmer is lazy...\n\n");
-    printf("Please enter a starting x-coordinate, starting y-coordinate, and length (in order): ");
-    scanf("%i %i %i", &startX, &startY, &length);
-    printf("Now enter whether the line is horizontal or not (a \'1\' is horizontal and a \'0\' is vertical): ");
-    scanf("%i", &horizontality);
+
+
+    //Let user decide what type of 'art' they would like to see...
+    printf("What type of \"art\" would you like to see today? Press 1 to draw a singular line or press 2 to see my initials: ");
+    scanf("%i", &artDecision);
+
+    if (artDecision == 1){
+
+        //Get user input for the ONE line they are allocated...
+        printf("\nYou may draw only ONE line because \"art\" is subjective and the programmer is lazy...\n\n");
+        printf("Please enter a starting x-coordinate, starting y-coordinate, and length (in order, space-separated, without commas): ");
+        scanf("%i %i %i", &startX, &startY, &length);
+        printf("Now enter whether the line is horizontal or not (a \'1\' is horizontal and a \'0\' is vertical): ");
+        scanf("%i", &horizontality);
+
+        //Start Curses
+        initscr();
+
+        //Start some color stuff for Curses
+        start_color();
+        init_pair(BLACK_WHITE, COLOR_BLACK, COLOR_WHITE);
+        init_pair(RED_BLUE, COLOR_RED, COLOR_BLUE);
+        wbkgd(stdscr, COLOR_PAIR(BLACK_WHITE));
+
+        //Draw the user's line and sign the work
+        drawLine(startX, startY, length, horizontality);
+        signWork();
+
+    } else if (artDecision == 2){
+
+        //Start Curses
+        initscr();
+
+        //Start some color stuff for Curses
+        start_color();
+        init_pair(BLACK_WHITE, COLOR_BLACK, COLOR_WHITE);
+        init_pair(WHITE_BLUE, COLOR_WHITE, COLOR_BLUE);
+        wbkgd(stdscr, COLOR_PAIR(BLACK_WHITE));
+
+        //Draw the user's line and sign the work
+        drawLine(startX, startY, length, horizontality);
+        signWork();
+
+    }
 
     //Start curses
-    initscr();  //! Required by curses to initialize
 
-    //Color Stuff
-    start_color();
-    init_pair(BLACK_WHITE, COLOR_BLACK, COLOR_WHITE);
-    init_pair(RED_BLUE, COLOR_RED, COLOR_BLUE);
-    wbkgd(stdscr, COLOR_PAIR(BLACK_WHITE));
-
-    /* SOME BULLSHIT HERE */
-    drawLine(startX, startY, length, horizontality);
-    signWork();
 
     //Wait for keystroke then end
-    move(20, 5);
+    move(23, 5);
     printw("Press a key to continue: ");
     getch();
 
@@ -61,24 +91,28 @@ int main(void){
 //Method definitions:
 void drawLine(int beginRow, int beginCol, int length, int isHorizontal){
 
+    attron(COLOR_PAIR(RED_BLUE));
+
     move(beginRow, beginCol);
     if (isHorizontal == 1) {
 
-        for (int i = 1; i < length; i++) {
-            printw("H");
+        for (int i = 1; i < length + 1; i++) {
+            printw(" ");
             move(beginRow, beginCol + i);
         }
 
     } else {
-        for (int i = 1; i < length; i++) {
-            printw("v");
-            move(beginRow, beginCol);
+
+        for (int i = 1; i < length + 1; i++) {
+            printw(" ");
+            move(beginRow + i, beginCol);
         }
     }
 
+    attroff(COLOR_PAIR(RED_BLUE));
 }
 
 void signWork(){
-    move(23, 5);
+    move(20, 5);
     printw("Creaded with Curses by David Petzold.\n");
 }
